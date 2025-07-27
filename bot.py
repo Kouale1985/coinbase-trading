1073afaf5477b3a6cfa685986336a92b39950a5c
Kouale1985 <alexander@kouznetsov.me>
2025-07-27 18:04:14 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import asyncio
 import pandas as pd
-from coinbase.advanced_trade.client import AsyncCoinbaseAdvancedTradeClient
+from coinbase.rest.client import AsyncAdvancedTradeClient
 from datetime import datetime
 
 # Load from environment
